b000d951a82474ce3a6580da636ca97795805ca2
victorcigano <viviconeto40@gmail.com>
2025-10-23 20:30:10 -0300

    Delete CardGeneratorVictorNicolauNeto directory

--- CardGeneratorVictorNicolauNeto/src/main/java/com/sinodal/CardGeneratorVictorNicolauNeto/model/YearMonthConverter.java
+++
@@ -1,6 +0,0 @@
-package com.sinodal.CardGeneratorVictorNicolauNeto.model;
-
-// Stub kept to avoid leftover compile errors from earlier edits. No-op.
-public final class YearMonthConverter {
-    private YearMonthConverter() {}
-}
